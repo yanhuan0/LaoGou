23abd946220f09a37584dc5b7adfe8ecb7d8664d
user <340359956@qq.com>
2021-12-23 23:13:23 +0800

    可变数量的参数重载

---
+++ src/practice2/ADHero1.java
@@ -0,0 +1,26 @@
+package practice2;
+
+public class ADHero1 extends Hero{
+        public void attack(){
+            System.out.println(name+"对"+" "+"进行了一次攻击");
+        }
+        public void attack(Hero...Heros){
+            for (int i=0;i<Heros.length;i++){
+                System.out.println(name+"攻击了"+Heros[i].name);
+            }
+        }
+        public static void main(String[] args) {
+
+           ADHero1 hero=new ADHero1();
+            hero.name="赏金猎人";
+
+            Hero h1=new Hero();
+            h1.name="盖伦";
+
+            Hero h2=new Hero();
+            h2.name="提莫";
+
+            hero.attack(h1);
+            hero.attack(h1,h2);
+        }
+}
