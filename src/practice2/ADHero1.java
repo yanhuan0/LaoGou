b47bfae24117bac747be93d1b1fd163cc1ded7e5
yanhuan0 <340359956@qq.com>
2021-12-24 16:26:26 +0800

    增加重载需求

--- src/practice2/ADHero1.java
+++ src/practice2/ADHero1.java
@@ -2,13 +2,21 @@ package practice2;
 
 public class ADHero1 extends Hero{
         public void attack(){
+
             System.out.println(name+"对"+" "+"进行了一次攻击");
         }
+
+        public void attack(Hero h1,Hero h2){
+            System.out.println(name+"攻击了"+h1.name+"和"+h2.name);
+        }
+
         public void attack(Hero...Heros){
             for (int i=0;i<Heros.length;i++){
                 System.out.println(name+"攻击了"+Heros[i].name);
             }
         }
+
+
         public static void main(String[] args) {
 
            ADHero1 hero=new ADHero1();
--- src/practice2/Support.java
+++ src/practice2/Support.java
@@ -1,4 +1,26 @@
 package practice2;
+//辅助英雄
+public class Support extends Hero{
+    public void heal(){
+        System.out.println(name+"为"+""+"加血");
+    }
+    public void heal(Hero h){
+        System.out.println(name+"为"+h.name+"加血");
+    }
+    public void heal(Hero h,int hp){
+        System.out.println(name+"为"+h.name+"加加了"+hp+"血量");
+    }
 
-public class Support {
+    public static void main(String[] args) {
+        Support support = new Support();
+        support.name="辅助英雄";
+
+        Hero h=new Hero();
+        h.name="盖伦";
+//        h.hp=50;
+
+        support.heal(h);
+        support.heal(h,50);
+
+    }
 }
