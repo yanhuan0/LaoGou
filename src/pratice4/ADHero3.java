859c48ae37b0cd8a028f1f82e0f010a38815cd18
yanhuan0 <340359956@qq.com>
2022-01-13 18:48:48 +0800

    跟着写的

--- src/pratice4/ADHero3.java
+++ src/pratice4/ADHero3.java
@@ -1,5 +1,10 @@
 package pratice4;
 
 public class ADHero3 {
-
+    public ADHero3(){
+        System.out.println("adhero3的构造方法");
+    }
+    public String name(){
+        return name();
+    }
 }
