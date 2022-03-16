226ba5667920d817c7ce6fb7618e213fee6a7e7f
yanhuan0 <340359956@qq.com>
2022-03-16 23:05:05 +0800

    提交代码

---
+++ src/practice4/AP1.java
@@ -0,0 +1,10 @@
+package practice4;
+
+public interface AP1 {
+    public static final int resistPphysic=100;
+
+    //resistPphysic即便没有显式的声明为public static final
+    //但依然默认为public static final
+
+    public void magicAttack();
+}
